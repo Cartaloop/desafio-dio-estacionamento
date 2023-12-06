a078b6b9d83efd25e1924d6fc70dac6b88797cc0
“Lucas <lucasrech00@gmail.com>
2023-12-05 23:18:15 -0300

    Fix the output error ")" in method RegisterVehicle and create a new folder for the PaymentRule

--- Models/Parking.cs
+++ Models/Parking.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DesafioFundamentos.Utilitaries;
 
 namespace DesafioFundamentos.Models
 {
@@ -12,6 +13,8 @@ namespace DesafioFundamentos.Models
         public double PricePerHour { get; set; }
         List<Vehicle>? RegisteredVehicles { get; set; }
 
+        
+        // creates a constructor that instantiates the Vehicle<list> class and gets values for the business rule
         public Parking()
         {
             RegisteredVehicles = new List<Vehicle>();
@@ -22,14 +25,20 @@ namespace DesafioFundamentos.Models
             Console.Clear();
         }
 
+        /// <summary>
+        /// Register an vehicle to the Vehicle list (RegisteredVehicle) based on his Plate(input from user).
+        /// </summary>
         public void RegisterVehicle()
         {
             Console.Write("Digite a placa do veículo para estacionar: ");
             string vehiclePlate = Console.ReadLine()!;
             RegisteredVehicles!.Add(new Vehicle(vehiclePlate));
-            Console.WriteLine($"Veículo de placa {vehiclePlate} adicionado)");
+            Console.WriteLine($"Veículo de placa {vehiclePlate} adicionado");
         }
 
+        /// <summary>
+        /// removes a vehicle based on the plate stored in the Vehicle list (RegisteredVehicle)
+        /// </summary>
         public void RemoveVehicle()
         {
             Console.Write("Informe a placa do veículo que deseja remover: ");
@@ -41,20 +50,21 @@ namespace DesafioFundamentos.Models
             {
                 Console.Write("Informe o tempo que o veículo ficou estacionado: ");
                 int timeParked = int.Parse(Console.ReadLine()!);
+                double totalToPay = PaymentRule.CalculateTime(timeParked, PricePerHour, InitialPrice);
+
                 Console.WriteLine($"O veículo {vehicleToRemove} foi removido com sucesso. " +
-                    $"Total a pagar de {CalculateTime(timeParked, PricePerHour, InitialPrice)}");
-                RegisteredVehicles?.Remove(vehicleToRemove);
+                    $"Total a pagar de {totalToPay}");
 
             }
             else
             {
                 Console.WriteLine("Veículo não encontrado na lista");
-                
             }
-                       
-
         }
 
+        /// <summary>
+        /// List all the indexes from RegisteredVehicle
+        /// </summary>
         public void ListVehicles()
         {
             if(RegisteredVehicles!.Count() > 0)
@@ -72,14 +82,5 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Nenhum veículo estacionado.");
             }
         }
-
-
-        private static string CalculateTime(int hours, double pricePerHour, double initialPrice)
-        {
-            double calcule = (pricePerHour * hours) + initialPrice;
-            return calcule.ToString("F2");
-        }
-          
-
     }
 }
---
+++ Utilitaries/PaymentRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioFundamentos.Utilitaries
+{
+    public class PaymentRule
+    {
+        /// <summary>
+        /// A method that contains the rule of payment
+        /// </summary>
+        /// <param name="hours">set an integer value defined by the hours spended at the parking</param>
+        /// <param name="pricePerHour"></param>
+        /// <param name="initialPrice"></param>
+        /// <returns></returns>
+        public static double CalculateTime(int hours, double pricePerHour, double initialPrice)
+        {
+            double calcule = (pricePerHour * hours) + initialPrice;
+            return calcule;
+        }
+    }
+}
